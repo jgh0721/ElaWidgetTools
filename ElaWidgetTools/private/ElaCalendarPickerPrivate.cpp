f09737746ae25257ff1397772b2c10bfbffe98a5
jgh0721 <jgh0721@gmail.com>
2026-04-29 08:39:56 +0900

    ElaCalendarPicker 가 ElaCalendar 를 표시할 때 공간이 부족하면 위쪽에 표시하도록 개선

--- ElaWidgetTools/private/ElaCalendarPickerPrivate.cpp
+++ ElaWidgetTools/private/ElaCalendarPickerPrivate.cpp
@@ -1,7 +1,9 @@
 #include "ElaCalendarPickerPrivate.h"
 
 #include <QDate>
+#include <QGuiApplication>
 #include <QPropertyAnimation>
+#include <QScreen>
 
 #include "ElaCalendar.h"
 #include "ElaCalendarPicker.h"
@@ -19,10 +21,30 @@ void ElaCalendarPickerPrivate::onCalendarPickerClicked()
 {
     Q_Q(ElaCalendarPicker);
     QPoint endPoint(q->mapToGlobal(QPoint((q->width() - _calendarPickerContainer->width()) / 2, q->height() + 5)));
+    int startYOffset = -10;
+
+    // 현재 위젯이 위치한 스크린의 가용 영역을 확인합니다.
+#if QT_VERSION >= QT_VERSION_CHECK(5, 14, 0)
+    QScreen* screen = q->screen();
+#else
+    QScreen* screen = QGuiApplication::screenAt(q->mapToGlobal(QPoint(0, 0)));
+#endif
+    if (screen)
+    {
+        // 캘린더 컨테이너가 화면 하단 영역을 벗어나는지 검사
+        if (endPoint.y() + _calendarPickerContainer->height() > screen->availableGeometry().bottom())
+        {
+            // 공간이 부족한 경우 피커 위젯의 상단으로 위치를 재조정합니다.
+            endPoint = q->mapToGlobal(QPoint((q->width() - _calendarPickerContainer->width()) / 2, -_calendarPickerContainer->height() - 5));
+            startYOffset = 10;
+        }
+    }
+
     QPropertyAnimation* showAnimation = new QPropertyAnimation(_calendarPickerContainer, "pos");
     showAnimation->setEasingCurve(QEasingCurve::OutCubic);
     showAnimation->setDuration(250);
-    showAnimation->setStartValue(QPoint(endPoint.x(), endPoint.y() - 10));
+    // 나타나는 애니메이션의 시작점 또한 캘린더가 열리는 방향에 맞춰 보정합니다.
+    showAnimation->setStartValue(QPoint(endPoint.x(), endPoint.y() + startYOffset));
     showAnimation->setEndValue(endPoint);
     _calendarPickerContainer->show();
     showAnimation->start(QAbstractAnimation::DeleteWhenStopped);
